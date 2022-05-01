8af06c0b92fdcd51a16c3177d9bd3d9945c61a7f
Lipa <lipsa1995@gmail.com>
2022-05-02 02:05:53 +0530

    Admin application views update

--- ahc_admin/views.py
+++ ahc_admin/views.py
@@ -58,7 +58,7 @@ class AddNewClient(CreateView):
 
 class AdminSignUpView(CreateView):
     model = User
-    form_class = SuperClientSignUpForm
+    form_class = AdminSignUpForm
     template_name = 'registration/signup_form.html'
 
     def get_context_data(self, **kwargs):
