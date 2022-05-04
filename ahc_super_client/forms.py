0a90ccbd240c997239c5afac39fe709dd4f96d82
Lipa <lipsa1995@gmail.com>
2022-05-04 08:26:07 +0530

    to take pull,commit by lipsa

--- ahc_super_client/forms.py
+++ ahc_super_client/forms.py
@@ -5,13 +5,17 @@ from ahc_app.models import User
 
 
 class AddClientForm(UserCreationForm):
-    username = forms.CharField(max_length=32, help_text='First name',widget=forms.TextInput(attrs={'placeholder': 'username'}))
-    first_name = forms.CharField(max_length=32, help_text='First name',widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
-    last_name = forms.CharField(max_length=32, help_text='Last name',widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
+    username = forms.CharField(max_length=32, help_text='First name',
+                               widget=forms.TextInput(attrs={'placeholder': 'username'}))
+    first_name = forms.CharField(max_length=32, help_text='First name',
+                                 widget=forms.TextInput(attrs={'placeholder': 'First Name'}))
+    last_name = forms.CharField(max_length=32, help_text='Last name',
+                                widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
     email = forms.EmailField(max_length=64,
-                             help_text='Enter a valid email address',widget=forms.TextInput(attrs={'placeholder': 'Email Address'}))
+                             help_text='Enter a valid email address',
+                             widget=forms.TextInput(attrs={'placeholder': 'Email Address'}))
     mobile_number = forms.IntegerField(
-        help_text='Enter a valid mobile number',widget=forms.TextInput(attrs={'placeholder': 'Mobile Number'}))
+        help_text='Enter a valid mobile number', widget=forms.TextInput(attrs={'placeholder': 'Mobile Number'}))
     password1 = forms.CharField(max_length=32, help_text='Password',
                                 widget=forms.TextInput(attrs={'placeholder': 'Password'}))
     password2 = forms.CharField(max_length=32, help_text='Retype Password',
@@ -27,5 +31,3 @@ class AddClientForm(UserCreationForm):
         if commit:
             user.save()
         return user
-
-
