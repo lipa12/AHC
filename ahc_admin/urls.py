6ddc2815fb4b06da1687063fdd8bdaf38bb156fb
Lipa <lipsa1995@gmail.com>
2022-05-08 10:32:22 +0530

    admin profile update

--- ahc_admin/urls.py
+++ ahc_admin/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('client_list/', views.client_list, name='client_list'),
     path('master_client_list/', views.master_client_list, name='master_client_list'),
     path('trade/', views.trade, name='trade'),
+    path('profile/', views.admin_profile, name = 'admin_profile'),
     path('client_profile/', views.client_profile, name='client_profile'),
     path('broker_profile/', views.broker_profile, name='broker_profile'),
 ]
