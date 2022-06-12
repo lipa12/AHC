be5140c5791923199978f0217cd79d5b72624fc2
alokkjnu <ry7069577@gmail.com>
2022-06-12 21:42:01 +0530

    AHC admin trading strategies update

--- ahc_admin/views.py
+++ ahc_admin/views.py
@@ -11,26 +11,29 @@ from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, ListView, UpdateView
 from ahc_app.decorators import broker_required
 from ahc_app.forms import AdminSignUpForm
-from ahc_client.models import NiftyBanknifty
+from ahc_client.models import NiftyBanknifty, TradeStrategies
 from ahc_super_client.forms import AddClientForm
 from ahc_app.models import User
 
 
 @login_required(login_url="loginuser")
 def index(request):
+    strategies_number = TradeStrategies.objects.all()
     # return render(request, 'ahc_app/pages/forms/add_client.html')
-    return render(request, 'ahc_app/ahc_admin.html')
+    return render(request, 'ahc_app/ahc_admin.html', {'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
 def admin_profile(request):
     profile = User.objects.filter(username=request.user.username)
-    return render(request, 'ahc_app/pages/profile/admin_profile.html', {'profile': profile})
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/profile/admin_profile.html', {'profile': profile,'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
 def admin_profile_update(request):
     profile = User.objects.filter(username=request.user.username)
+    strategies_number = TradeStrategies.objects.all()
     if request.method == "POST":
         user = User.objects.get(username=request.user.username)
         user.first_name = request.POST.get('firstname')
@@ -41,7 +44,7 @@ def admin_profile_update(request):
         user.save()
 
         return redirect('ahc_admin:index')
-    return render(request, 'ahc_app/pages/forms/admin_profile_update.html', {'profile': profile})
+    return render(request, 'ahc_app/pages/forms/admin_profile_update.html', {'profile': profile,'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
@@ -53,41 +56,52 @@ def trade_data(request):
 @login_required(login_url="loginuser")
 def add_client(request):
     user = request.user.username
-    return render(request, 'ahc_app/pages/forms/add_client.html')
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/forms/add_client.html',{'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
 def client_list(request):
     client_list = User.objects.filter(is_client=True)
-    return render(request, 'ahc_app/pages/tables/admin_client_list.html', {'client_list': client_list})
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/tables/admin_client_list.html', {'client_list': client_list,'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
 def master_client_list(request):
     master_client_list = User.objects.filter(is_super_client=True)
-    return render(request, 'ahc_app/pages/tables/master_client_list.html', {'master_client_list': master_client_list})
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/tables/master_client_list.html', {'master_client_list': master_client_list,'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
-def trade(request):
-    return render(request, 'ahc_app/pages/tables/trades.html')
+def trade(request, strategies):
+    data = NiftyBanknifty.objects.filter()[1:]
+    strategies_number = TradeStrategies.objects.all()
+    strategies_data = TradeStrategies.objects.filter(strategies=strategies)
+    return render(request, 'ahc_app/pages/tables/trades.html',
+                  {'stock_data': list(data.values()), 'strategies_number': strategies_number,
+                   'strategies_data': strategies_data})
 
 
 @login_required(login_url="loginuser")
 def client_profile(request, profile_id):
     profile = User.objects.filter(id=profile_id)
-    return render(request, 'ahc_app/pages/profile/profile_details.html', {'profile': profile})
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/profile/profile_details.html', {'profile': profile,'strategies_number': strategies_number})
 
 
 @login_required(login_url="loginuser")
 def master_client_profile(request, profile_id):
     profile = User.objects.filter(id=profile_id)
-    return render(request, 'ahc_app/pages/profile/profile_details.html', {'profile': profile})
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/profile/profile_details.html', {'profile': profile,'strategies_number': strategies_number})
 
 
 def profile_update(request, profile_id):
     profile = User.objects.filter(id=profile_id)
-    return render(request, 'ahc_app/pages/forms/client_profile_update.html', {'profile': profile})
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/forms/client_profile_update.html', {'profile': profile,'strategies_number': strategies_number})
 
 
 def save_profile_data(request):
@@ -106,7 +120,8 @@ def save_profile_data(request):
 
 @login_required(login_url="loginuser")
 def broker_profile(request):
-    return render(request, 'ahc_app/pages/profile/broker_profile.html')
+    strategies_number = TradeStrategies.objects.all()
+    return render(request, 'ahc_app/pages/profile/broker_profile.html',{'strategies_number': strategies_number})
 
 
 class AddNewClient(CreateView):
